590642dea6ad1fda35522779fe3d50cba3d8fadb
ashwani-singh-st18 <ashwani-singh-st18@staytuned.website>
2026-04-21 13:25:55 +0530

    for-in loop for iterating objects

---
+++ iterations/for_in.js
@@ -0,0 +1,12 @@
+const object = {
+    js : "javascript",
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in object) {
+    console.log(`${key} shortcut is for ${object[key]}`)   
+}
+    
+    
