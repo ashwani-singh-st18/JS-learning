15f335aec397907e532a5a7ace8ee7b4289fc958
ashwani-singh-st18 <ashwani-singh-st18@staytuned.website>
2026-04-21 13:09:25 +0530

    for of loop

---
+++ iterations/for_of.js
@@ -0,0 +1,16 @@
+// for of
+
+const arr = [1,2,3,4,5]
+
+for (const element of arr) {
+    console.log(element);
+    
+}
+
+
+const greetings = "Hello World"
+for(const greet of greetings){
+    console.log(greet);
+}
+
+
